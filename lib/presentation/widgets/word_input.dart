525d9dedb243bfc2e25e09a520e253b5a69cad33
adrija1110 <adrija.chakraborty@thoughtworks.com>
2024-11-17 01:26:52 +0530

    Refactor. question and guess input segments

--- lib/presentation/widgets/word_input.dart
+++ lib/presentation/widgets/word_input.dart
@@ -1,29 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-
-// import '../cubit/game_cubit.dart';
-
-// class PasswordInput extends StatelessWidget {
-//   const PasswordInput({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     TextEditingController controller = TextEditingController();
-
-//     return Padding(
-//       padding: const EdgeInsets.all(16.0),
-//       child: TextField(
-//         decoration: const InputDecoration(labelText: 'Guess the password'),
-//         controller: controller,
-//         onSubmitted: (value) {
-//           context.read<GameCubit>().checkGuess(value);
-//           controller.clear();
-//         },
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubit/game_cubit.dart';
